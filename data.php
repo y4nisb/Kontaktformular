41a2ac24e97790f7912d604bf25df63a294ea0ec
yanisb <yanis.buelow@stud.kftg.ch>
2023-08-29 20:55:23 +0200

    hinzufügen des forms und der variablen
    variablen hinzugefügt

--- data.php
+++ data.php
@@ -1,4 +1,25 @@
 <?php
-    echo $_POST["username"] . "<br>"; 
-    echo "{$_POST["password"]} <br>";
-?>
+    $Anrede = $_POST["anrede"];
+    $Vorname = $_POST["Vorname"];
+    $Nachname = $_POST["Nachname"];
+    $Tag = $_Post["Tag"];
+    echo $Tag;
+    $Monat = $_POST["Monat"];
+    $Jahr = $_POST["Jahr"];
+    $Telephon = $_POST["Telefonnummer"];
+    $Email = $_POST["E-Mail"];
+    $Strasse = $_POST["Strasse"];
+    $Stadt = $_POST["Stadt"];
+    $PLZ = $_POST["PLZ"];
+    $Land = $_POST["Land"];
+    $text = $_POST["text"];
+   
+
+   
+    
+    echo("vielen Dank")
+
+
+    
+  
+  ?>
--- index.php
+++ index.php
@@ -9,27 +9,23 @@
     <form action="data.php" method="post">
         <div class="Anrede">
       <label>Anrede</label><br /><br />
-        <input type="radio" id="frau" name="fav_language" value="Frau">
-        <label for="Frau">Frau</label><br>
-        <input type="radio" id="herr" name="fav_language" value="Herr">
-        <label for="Herr">Herr</label><br>
-        <input type="radio" name="Divers" value="Divers" />
-        <label for="Divers">Divers</label><br>
+        <input type="radio" id="frau" name="anrede" value="Frau">Frau <br>
+        <input type="radio" id="herr" name="anrede" value="Herr">Herr<br>
+        <input type="radio" name="anrede" value="Divers" />Anders<br>
         </div>
       <div class="Name">
       <label>Vollständiger Name</label><br />
-        <input type="text" name="Vorname" /><br />
-        <label for= "Vorname">Vorname</label><br />
-        <input type="text" name="Nachname" /><br />
-        <label for= "Nachname">Nachname</label><br />
+        <input type="text" name="Vorname" />Vorname<br />
+        <input type="text" name="Nachname" />Nachname<br />
       </div>
         <div class="Alter">
         <select name="Tag" id="tag">
             <script>
                 for (let i = 1; i <= 31; i++) {
-                    document.write("<option value='" + i + "'>" + i + "</option>");
+                    document.write("<option name='Tag' value='" + i + "'>" + i + " </option>");
                 }
             </script>  
+         
         </select>
         <select name="Monat" id="monat">
             <script>
@@ -46,6 +42,31 @@
             </script>
         </select>
         </div>
+        <div class="Email">
+        <label>E-Mail</label><br />
+        <input type="email" name="E-Mail" placeholder="max.muster@gmail.com"/><br />
+        </div>
+        <div class="Telefonnummer">
+        <label>Telefonnummer</label><br />
+        <input type="tel" name="Telefonnummer" placeholder="070 999 99 99"/><br />
+        </div>
+        <div class= "Adresse">
+        <label>Adresse</label><br />
+        <label for="Strasse">Strasse und Hausnummer</label><br />
+        <input type="text" name="Strasse" /><br />
+        <label for="Stadt">Stadt</label><br />
+        <input type="text" name="Stadt"><br>
+        <label for="PLZ">PLZ</label><br />
+        <input type="nummber" name="PLZ" /><br />
+        <label for="Land" >Land</label><br>
+        <input type="text" name="Land">
+        </div>
+        <div class="Text">
+            <label for="Text">Text</label><br>
+            <input type="content" name="text">
+        </div>
+             
+
       <input type="submit" value="Abschicken" />
     </form>
   </body>
