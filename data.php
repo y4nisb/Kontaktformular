0065692932b5a4fa081a451ee3630c4ca7f335ab
yanisb <yanis.buelow@stud.kftg.ch>
2023-09-04 20:53:02 +0200

    finish
    fully functional
    probably not safe

--- data.php
+++ data.php
@@ -1,25 +1,83 @@
 <?php
-    $Anrede = $_POST["anrede"];
-    $Vorname = $_POST["Vorname"];
-    $Nachname = $_POST["Nachname"];
-    $Tag = $_Post["Tag"];
-    echo $Tag;
-    $Monat = $_POST["Monat"];
-    $Jahr = $_POST["Jahr"];
-    $Telephon = $_POST["Telefonnummer"];
-    $Email = $_POST["E-Mail"];
+$count = 0;
+include_once "datenbank.php";
+
+
+    if(empty($_POST["anrede"])){
+      echo "Anrede wird benötigt". "<br>";
+      $count++;
+    }else{
+      $Anrede= $_POST["anrede"];
+    } 
+    if(empty($_POST["Vorname"])){
+      $count++;
+     echo "Vorname wird benötigt" . "<br>";
+    }else{
+      $Vorname = $_POST["Vorname"];
+    }
+  
+    if(empty($_POST["Nachname"])){
+      echo "Nachname wird benötigt" . "<br>";
+      $count++;
+    }else{
+      $Nachname= $_POST["Nachname"];
+    }
+    
+    $Telephon= $_POST["Telefonnummer"];
+    
+
+  
+ 
+    if(empty($_POST["E-Mail"])){
+     echo "E-Mail wird benötigt" . "<br>";
+     $count++;
+    }else{
+      $Email= $_POST["E-Mail"];
+    }
+    
     $Strasse = $_POST["Strasse"];
     $Stadt = $_POST["Stadt"];
     $PLZ = $_POST["PLZ"];
     $Land = $_POST["Land"];
-    $text = $_POST["text"];
-   
+    
+    if(empty($_POST["text"])){
+      echo "Text wird benötigt" . "<br>";
+      $count++;
+    }else{
+      $text= $_POST["text"];
+    }
+    $sql= "insert into tdaten (Anrede, Vorname, Nachname, Telefon, Email, Strasse, Stadt, Plz, Land, Nachricht) values ('$Anrede', '$Vorname', '$Nachname',  '$Telephon', '$Email', '$Strasse', '$Stadt', '$PLZ', '$Land', '$text')";
+
+      if ($count === 0) {
+          echo "Vielen Dank für Ihre Eingabe" . "<br>";
+          mysqli_query($db_conn, $sql);
+  }else{
+    ?>
+    <button type="submit" name="submit">Zurück</button>
+<?php
+  }
+  
+  
 
    
-    
-    echo("vielen Dank")
 
 
     
   
   ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form action="index.php" method="post">
+  
+</form>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,74 +1,68 @@
 <!DOCTYPE html>
 <html lang="de">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Kontaktformular</title>
-  </head>
-  <body>
-    <form action="data.php" method="post">
-        <div class="Anrede">
-      <label>Anrede</label><br /><br />
-        <input type="radio" id="frau" name="anrede" value="Frau">Frau <br>
-        <input type="radio" id="herr" name="anrede" value="Herr">Herr<br>
-        <input type="radio" name="anrede" value="Divers" />Anders<br>
-        </div>
-      <div class="Name">
-      <label>Vollständiger Name</label><br />
-        <input type="text" name="Vorname" />Vorname<br />
-        <input type="text" name="Nachname" />Nachname<br />
-      </div>
-        <div class="Alter">
-        <select name="Tag" id="tag">
-            <script>
-                for (let i = 1; i <= 31; i++) {
-                    document.write("<option name='Tag' value='" + i + "'>" + i + " </option>");
-                }
-            </script>  
-         
-        </select>
-        <select name="Monat" id="monat">
-            <script>
-                for (let i = 1; i <= 12; i++) {
-                    document.write("<option value='" + i + "'>" + i + "</option>");
-                }
-            </script>
-        </select>
-        <select name="Jahr" id="jahr">
-            <script>
-                for (let i = 1900; i <= 2021; i++) {
-                    document.write("<option value='" + i + "'>" + i + "</option>");
-                }
-            </script>
-        </select>
-        </div>
-        <div class="Email">
-        <label>E-Mail</label><br />
-        <input type="email" name="E-Mail" placeholder="max.muster@gmail.com"/><br />
-        </div>
-        <div class="Telefonnummer">
-        <label>Telefonnummer</label><br />
-        <input type="tel" name="Telefonnummer" placeholder="070 999 99 99"/><br />
-        </div>
-        <div class= "Adresse">
-        <label>Adresse</label><br />
-        <label for="Strasse">Strasse und Hausnummer</label><br />
-        <input type="text" name="Strasse" /><br />
-        <label for="Stadt">Stadt</label><br />
-        <input type="text" name="Stadt"><br>
-        <label for="PLZ">PLZ</label><br />
-        <input type="nummber" name="PLZ" /><br />
-        <label for="Land" >Land</label><br>
-        <input type="text" name="Land">
-        </div>
-        <div class="Text">
-            <label for="Text">Text</label><br>
-            <input type="content" name="text">
-        </div>
-             
+    <link rel="stylesheet" href="style.css" />
+</head>
 
-      <input type="submit" value="Abschicken" />
-    </form>
-  </body>
+<body>
+    <div class="form">
+        <h1>Kontaktformular</h1>
+        <p>* Pflichtfelder</p>
+        <form action="data.php" method="post">
+            <label>* Anrede</label><br />
+            <input type="radio" id="frau" name="anrede" value="Frau">Frau
+            <input type="radio" id="herr" name="anrede" value="Herr">Herr
+            <input type="radio" name="anrede" value="Divers">Anders<br><br>
+
+            <label>Vollständiger Name</label><br />
+            <input type="text" name="Vorname" placeholder="Vorname" required>
+            <input type="text" name="Nachname" placeholder="Nachname" required><br>
+
+            <label>Geburtsdatum</label><br />
+            <select name="Tag" id="tag">
+                <script>
+                    for (let i = 1; i <= 31; i++) {
+                        document.write("<option value='" + i + "'>" + i + " </option>");
+                    }
+                </script>
+            </select>
+            <select name="Monat" id="monat">
+                <script>
+                    for (let i = 1; i <= 12; i++) {
+                        document.write("<option value='" + i + "'>" + i + "</option>");
+                    }
+                </script>
+            </select>
+            <select name="Jahr" id="jahr">
+                <script>
+                    for (let i = 1900; i <= 2023; i++) {
+                        document.write("<option value='" + i + "'>" + i + "</option>");
+                    }
+                </script>
+            </select><br>
+
+            <label>* E-Mail</label><br />
+            <input type="email" name="E-Mail" placeholder="max.muster@gmail.com" required><br>
+
+            <label>Telefonnummer</label><br />
+            <input type="tel" name="Telefonnummer" placeholder="070 999 99 99"><br>
+
+            <label>Adresse</label><br />
+            <input type="text" name="Strasse" placeholder="Strasse und Hausnummer"><br>
+            <input type="text" name="Stadt" placeholder="Stadt"><br>
+            <input type="number" name="PLZ" placeholder="PLZ"><br>
+            <input type="text" name="Land" placeholder="Land"><br>
+
+            <label for="Text">* Text</label><br>
+            <textarea name="text" rows="4" required></textarea><br>
+
+            <input type="submit" value="Abschicken">
+        </form>
+    </div>
+</body>
+
 </html>
-
